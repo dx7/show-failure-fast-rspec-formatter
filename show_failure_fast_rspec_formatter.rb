c3db6ddbb10513e7319aed3d7d4860bf556a4069
dx7 <dx7@protonmail.ch>
2017-10-24 16:25:34 -0200

    update terminal-notifier command

--- show_failure_fast_rspec_formatter.rb
+++ show_failure_fast_rspec_formatter.rb
@@ -46,7 +46,7 @@ class ShowFailureFastRspecFormatter < RSpec::Core::Formatters::ProgressFormatter
     def system_notification(message, title, subtitle)
       if RUBY_PLATFORM.match(/darwin/)
         #`osascript -e 'display notification "#{message}" with title "#{title}"'`
-        `terminal-notifier -group 'rspec-#{project_name}' -title '#{title}' -subtitle '#{subtitle}' -message '#{message}' -activate 'com.googlecode.iterm2' -sender 'com.googlecode.iterm2'`
+        `terminal-notifier -group 'rspec-#{project_name}' -title '#{title}' -subtitle '#{subtitle}' -message '#{message}'`
       end
     end
 end
